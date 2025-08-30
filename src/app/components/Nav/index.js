ee046b144160bbdf90b3d8289d47328fb4335f14
Michelle <mliuu3259@gmail.com>
2025-08-30 17:48:41 -0400

    mobile view

--- src/app/components/Nav/index.js
+++ src/app/components/Nav/index.js
@@ -1,22 +1,48 @@
-import styles from './index.module.scss';
+'use client';
 import Link from 'next/link';
+import { useState } from 'react';
+import styles from './index.module.scss';
+import cn from 'classnames';
 
 const Nav = ({ children }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className={styles.container}>
-      <nav className={styles.nav}>
-        <h2 className={styles.name}>
-          <Link href="/">JULIE ROSTOCK</Link>
-        </h2>
-        <ul className={styles.navItems}>
+      <nav className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ''}`}>
+        <div className={styles.header}>
+          <h2 className={styles.name}>
+            <Link href="/">JULIE ROSTOCK</Link>
+          </h2>
+          <button
+            className={cn(styles.hamburger, isMenuOpen && styles.open)}
+            onClick={toggleMenu}
+            aria-label="Toggle menu"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+        <ul className={`${styles.navItems} ${isMenuOpen ? styles.show : ''}`}>
           <li className={styles.navItem}>
-            <Link href="/">Work</Link>
+            <Link href="/" onClick={() => setIsMenuOpen(false)}>
+              Work
+            </Link>
           </li>
           <li className={styles.navItem}>
-            <Link href="/cv">CV</Link>
+            <Link href="/cv" onClick={() => setIsMenuOpen(false)}>
+              CV
+            </Link>
           </li>
           <li className={styles.navItem}>
-            <Link href="/about">About</Link>
+            <Link href="/about" onClick={() => setIsMenuOpen(false)}>
+              About
+            </Link>
           </li>
         </ul>
       </nav>
